a8649f160460de918ac6c342a974a34cb987be42
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 16:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function BoardMember (name, homeState, training) {
-  this.name = name 
-  this.homeState = homeState 
+  this.name = name
+  this.homeState = homeState
   this.training = training
   this.veto = function () {
     console.log('No, I must disagree')
   }
   this.approve = function () {
-    console.log('You can do that!')
+    return 'You can do that!'
   }
   this.doCharity = function () {
-    console.log('I like to help people.')
+    return 'I like to help people.'
   }
-  
+  this.releasePressStatement = function () {
+    return 'You will see great things from Scuber.'
+  }
 }
